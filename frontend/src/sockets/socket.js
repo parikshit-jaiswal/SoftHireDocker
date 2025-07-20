2d9f2e4407698173fd446c66708468a8971b7afd
parikshit-jaiswal <parikshitjaiswal82@gmail.com>
2025-07-20 12:59:38 +0530

    fix: Correct server URL by removing '/api' segment in socket connection

--- frontend/src/sockets/socket.js
+++ frontend/src/sockets/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_SERVER_URL, {
+const socket = io(import.meta.env.VITE_SERVER_URL.replace('/api', ''), {
     withCredentials: true,
     autoConnect: false,
     transports: ['websocket'],
